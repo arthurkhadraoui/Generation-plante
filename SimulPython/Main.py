a563c54e6264da55886b2cd33e5ff950d5ec96be
Arthur Khadraoui-Nicod <49439975+artoche10@users.noreply.github.com>
2022-01-22 17:07:40 +0100

    Removed some images + added format choice

--- SimulPython/Main.py
+++ SimulPython/Main.py
@@ -5,7 +5,8 @@ from ReadImageParam import ReadImageParam
 
 for i in range(ReadImageParam()[2]):
     Matrice,MatriceLog = MainBranch()
-    nomImage="Img-0"+str(i+1)+".png"
+    format = ".png"
+    nomImage="Img-0"+str(i+1)+format
     nomLog = "Log-0" + str(i+1)
     export(Matrice,MatriceLog,nomImage,nomLog)
     print("Image ",i+1," generée")
